d595c425fb4ffdc5c8fef589007e48b72292595b
travisbennett03 <tbennett1996@yahoo.com>
2023-05-04 13:36:20 -0500

    Add project files.

---
+++ OpenWeatherAPI/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace OpenWeatherMapAPI
+{
+
+
+    class program
+    {
+
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+
+            Console.WriteLine("Please enter your API Key:");
+            var api_key = Console.ReadLine();
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Please enter the city name: ");
+                var city_name = Console.ReadLine();
+                var weatherURL = $"http://api.openweathermap.org/data/2.5/weather?q={city_name}&units=imperial&appid={api_key}";
+
+                var response = client.GetStringAsync(weatherURL).Result;
+                var formattedResponse = JObject.Parse(response).GetValue("main").ToString();
+                Console.WriteLine(formattedResponse);
+                Console.WriteLine();
+                Console.WriteLine("Would you like to choose a different city?");
+                var userInput = Console.ReadLine();
+                Console.WriteLine();
+                if (userInput.ToLower() == "no")
+                {
+                    break;
+                }
+                
+            }
+
+
+
+        }
+
+
+    }
+}
+
